44d64c0513c463815e2088826850491a3dbc2bab
박상혁 <psh161@naver.com>
2023-11-26 16:43:24 +0900

    fix : hilt 오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 fun readProperties(propertiesFile: File) = Properties().apply {

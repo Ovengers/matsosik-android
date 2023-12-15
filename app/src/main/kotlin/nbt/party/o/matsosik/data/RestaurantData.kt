c6b1cf633f3926badc02005dbbba190d8732ddfa
박상혁 <psh161@naver.com>
2023-12-15 22:47:40 +0900

    feat : 음식점 정보

--- app/src/main/kotlin/nbt/party/o/matsosik/data/RestaurantData.kt
+++ app/src/main/kotlin/nbt/party/o/matsosik/data/RestaurantData.kt
@@ -6,6 +6,7 @@ package nbt.party.o.matsosik.data
 data class RestaurantData(
     val id: Long,               // 음식점 ID
     val name: String,           // 음식점 이름
+    val address: String,        // 음식점 주소
     val latitude: Double,       // 음식점 위도
     val longitude: Double       // 음식점 경도
 )

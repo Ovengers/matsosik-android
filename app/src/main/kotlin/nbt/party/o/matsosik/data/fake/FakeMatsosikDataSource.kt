f8042fe2f3e42692c71ac7785b5774f1b6c3719c
박상혁 <psh161@naver.com>
2023-12-20 18:04:20 +0900

    feat : FakeData 추가 및 초기값 설정
    - 쌰오짠 추가
    - 양포식당 주소 및 위도 경도 수정
    - 마제스터 시티로 초기값 설정

--- app/src/main/kotlin/nbt/party/o/matsosik/data/fake/FakeMatsosikDataSource.kt
+++ app/src/main/kotlin/nbt/party/o/matsosik/data/fake/FakeMatsosikDataSource.kt
@@ -5,7 +5,14 @@ import nbt.party.o.matsosik.data.RestaurantData
 
 class FakeMatsosikDataSource : MatsosikDataSource {
     private val mockRestaurants = listOf(
-        RestaurantData(0, "양포식당", "address", 37.490269801827, 127.0009402539781)
+        RestaurantData(0, "양포식당", "서울특별시 서초구 서초동 1502-5", 37.4894743, 127.0068655),
+        RestaurantData(
+            1,
+            "샤오짠",
+            "아파트 서초구 상가 반포대로21길 27 서울특별시 화원그랑빌 1층 108호",
+            37.4870838,
+            127.0082748
+        )
     )
 
     override suspend fun getRestaurants(): List<RestaurantData> {
--- app/src/main/kotlin/nbt/party/o/matsosik/ui/map/MapScreen.kt
+++ app/src/main/kotlin/nbt/party/o/matsosik/ui/map/MapScreen.kt
@@ -8,10 +8,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.compose.CameraPositionState
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.Marker
 import com.naver.maps.map.compose.MarkerState
 import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
 import nbt.party.o.matsosik.data.RestaurantData
 
 @OptIn(ExperimentalNaverMapApi::class)
@@ -20,9 +23,19 @@ fun MapScreen(
     modifier: Modifier = Modifier,
     vm: MapViewModel = hiltViewModel()
 ) {
+
+    val seoul = LatLng(37.489972, 127.0051895)
+    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
+        // 카메라 초기 위치를 설정합니다.
+        position = CameraPosition(seoul, 15.0)
+    }
     NaverMap(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
     ) {
+
+
+
         val restaurants = vm.restaurants.collectAsState()
         restaurants.value.forEach { restaurantData: RestaurantData ->
             Marker(

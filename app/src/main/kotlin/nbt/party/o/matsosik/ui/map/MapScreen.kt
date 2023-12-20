d1a5048eeb7fded2da86da608a40fc91b87000ac
박상혁 <psh161@naver.com>
2023-12-20 17:46:54 +0900

    feat : 음식점 정보 가져오는 로직 추가
    - Fake DataSource에서 가져오는 로직 추가

--- app/src/main/kotlin/nbt/party/o/matsosik/ui/map/MapScreen.kt
+++ app/src/main/kotlin/nbt/party/o/matsosik/ui/map/MapScreen.kt
@@ -2,18 +2,23 @@ package nbt.party.o.matsosik.ui.map
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.NaverMap
+import timber.log.Timber
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun MapScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    vm: MapViewModel = hiltViewModel()
 ) {
     NaverMap(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
-
+        val restaurants = vm.restaurants.collectAsState()
+        Timber.i("restaurants : $restaurants")
     }
 }
---
+++ app/src/main/kotlin/nbt/party/o/matsosik/ui/map/MapViewModel.kt
@@ -0,0 +1,34 @@
+package nbt.party.o.matsosik.ui.map
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import nbt.party.o.matsosik.data.RestaurantData
+import nbt.party.o.matsosik.data.repo.MatsosikRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class MapViewModel @Inject constructor(
+    private val matsosikRepository: MatsosikRepository
+) : ViewModel() {
+
+    private val _restaurants: MutableStateFlow<List<RestaurantData>> =
+        MutableStateFlow(listOf())
+    val restaurants: StateFlow<List<RestaurantData>>
+        get() = _restaurants.asStateFlow()
+
+    init {
+        fetchRestaurants()
+    }
+
+    private fun fetchRestaurants() {
+        viewModelScope.launch {
+            val restaurants = matsosikRepository.getRestaurants()
+            _restaurants.emit(restaurants)
+        }
+    }
+}

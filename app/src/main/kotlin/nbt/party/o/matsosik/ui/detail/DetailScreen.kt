07c8cfbd7f4701e82e900e95940373eb8e3936e2
박상혁 <psh161@naver.com>
2024-02-05 23:42:51 +0900

    feat : 상세화면 Header 영역 구현
    - 상세화면 Image, Back 버튼 추가

---
+++ app/src/main/kotlin/nbt/party/o/matsosik/ui/detail/DetailScreen.kt
@@ -0,0 +1,60 @@
+package nbt.party.o.matsosik.ui.detail
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import nbt.party.o.matsosik.R
+import nbt.party.o.matsosik.ui.common.SystemThemeSurface
+import nbt.party.o.matsosik.ui.preview.DarkLightModePreview
+
+
+
+@Composable
+fun DetailHeader(
+    thumbnailImageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .aspectRatio(16f / 9f)
+    ) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = thumbnailImageUrl,
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+        )
+
+        Image(
+            modifier = Modifier
+                .padding(8.dp)
+                .size(32.dp)
+                .clickable { },
+            contentScale = ContentScale.Inside,
+            painter = painterResource(R.drawable.ic_arrow_back_24dp),
+            contentDescription = stringResource(id = R.string.back),
+        )
+    }
+}
+
+@DarkLightModePreview
+@Composable
+fun DetailHeaderPreview() {
+    SystemThemeSurface {
+        val url =
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzExMTdfMjUz%2FMDAxNzAwMTkyNTYxNjUy.-FS-UOMm61Q8veLrEMIvQHVrgQzd9IxvbEQQEbNh50Ig.h5S23QDcFtY8hQWGHG2Um4sAUTRDA-oiX0Sq0tJLxM8g.JPEG.a_zum_koya%2F20231117%25A3%25DF114135.jpg&type=sc960_832"
+        DetailHeader(url)
+    }
+
+}

745fb0484c07d6357d1edda96b9d33d4c6492bf3
박상혁 <psh161@naver.com>
2024-02-11 22:31:42 +0900

    feat : 상세화면 Title 추가

--- app/src/main/kotlin/nbt/party/o/matsosik/ui/detail/DetailScreen.kt
+++ app/src/main/kotlin/nbt/party/o/matsosik/ui/detail/DetailScreen.kt
@@ -7,17 +7,20 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import nbt.party.o.matsosik.R
 import nbt.party.o.matsosik.ui.common.SystemThemeSurface
 import nbt.party.o.matsosik.ui.preview.DarkLightModePreview
-
 
 
 @Composable
@@ -43,9 +46,22 @@ fun DetailHeader(
                 .clickable { },
             contentScale = ContentScale.Inside,
             painter = painterResource(R.drawable.ic_arrow_back_24dp),
-            contentDescription = stringResource(id = R.string.back),
+            contentDescription = stringResource(id = R.string.back)
         )
     }
+}
+
+@Composable
+fun DetailTitle(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        modifier = modifier,
+        text = title,
+        fontWeight = FontWeight.Bold,
+        style = MaterialTheme.typography.titleLarge
+    )
 }
 
 @DarkLightModePreview
@@ -56,5 +72,13 @@ fun DetailHeaderPreview() {
             "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzExMTdfMjUz%2FMDAxNzAwMTkyNTYxNjUy.-FS-UOMm61Q8veLrEMIvQHVrgQzd9IxvbEQQEbNh50Ig.h5S23QDcFtY8hQWGHG2Um4sAUTRDA-oiX0Sq0tJLxM8g.JPEG.a_zum_koya%2F20231117%25A3%25DF114135.jpg&type=sc960_832"
         DetailHeader(url)
     }
+}
 
+@DarkLightModePreview
+@Composable
+fun DetailTitlePreview() {
+    SystemThemeSurface {
+        val title = "우야(장어덮밥)"
+        DetailTitle(title)
+    }
 }

f8180500afe3713749080a62242460340855bf1b
박상혁 <psh161@naver.com>
2024-03-03 16:57:16 +0900

    MS-03 : VerticalSpacer, HorizontalSpacer CommonUI 추가

--- app/src/main/kotlin/nbt/party/o/matsosik/ui/common/CommonUI.kt
+++ app/src/main/kotlin/nbt/party/o/matsosik/ui/common/CommonUI.kt
@@ -1,8 +1,13 @@
 package nbt.party.o.matsosik.ui.common
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import nbt.party.o.matsosik.ui.theme.MatsosikTheme
 
 @Composable
@@ -13,3 +18,9 @@ fun SystemThemeSurface(
         Surface(color = MaterialTheme.colorScheme.background, content = content)
     }
 }
+
+@Composable
+fun VerticalSpacer(size: Dp) = Spacer(modifier = Modifier.height(size))
+
+@Composable
+fun HorizontalSpacer(size: Dp) = Spacer(modifier = Modifier.width(size))

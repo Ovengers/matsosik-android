d640898378f7846349958622a99a27a4af142f5a
박상혁 <psh161@naver.com>
2024-03-17 16:29:58 +0900

    MS-09 : Import 제거

--- app/src/main/kotlin/nbt/party/o/matsosik/ui/main/RestaurantViewModel.kt
+++ app/src/main/kotlin/nbt/party/o/matsosik/ui/main/RestaurantViewModel.kt
@@ -3,7 +3,6 @@ package nbt.party.o.matsosik.ui.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
